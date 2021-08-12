0182345f03bcc1bf622e7a6ccf5fbe170a821367
gongul <projectgongul@gmail.com>
2021-08-12 22:08:53 +0900

    피드 접근 권한 필드 추가

--- src/feed/models.py
+++ src/feed/models.py
@@ -7,10 +7,17 @@ from user.models import User
 # Create your models here.
 
 
+class AcessPermissionChoices(models.IntegerChoices):
+    PRIVATE = 0, '비공개'
+    PUBLIC = 1, '전체 공개'
+    FRIEND = 2, '친구 공개'
+
+
 class Feed(models.Model):
-    title = models.CharField(max_length=100)
-    thumbnail = models.CharField(max_length=100)
-    registration_date = models.DateTimeField(default=timezone.now)
-    contents = models.CharField(max_length=2000)
-    is_show = models.BooleanField(default=True)
+    title = models.CharField('제목', max_length=100)
+    thumbnail = models.CharField('썸네일', max_length=100)
+    registration_date = models.DateTimeField('피드 등록일', default=timezone.now)
+    contents = models.CharField('본문', max_length=2000)
+    access_permission = models.SmallIntegerField('피드 접근 권한', default=AcessPermissionChoices.PUBLIC,
+                                                 choices=AcessPermissionChoices.choices)
     user = models.ForeignKey(User, on_delete=CASCADE)
